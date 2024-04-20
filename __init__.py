e1af56e5fdcaa794e47c47b04a070c68c90783a3
DarKDinDoN <maxime.chicault@gmail.com>
2024-04-20 15:17:20 +0200

    Validate configuration

--- __init__.py
+++ __init__.py
@@ -12,6 +12,29 @@ def readConfigFile(file_path):
         return yaml.safe_load(file)
 
 
+# Validate checkpoint configuration
+def validateCheckpointConfig(config):
+    if not {"steps_total", "cfg", "sampler_name", "scheduler_name"} <= config.keys():
+        raise Exception(
+            "Automatic checkpoint configuration: missing key(s)/value(s) in the configuration file!")
+
+    if not isinstance(config["steps_total"], int) or config["steps_total"] < 1 or config["steps_total"] > MAX_RESOLUTION:
+        raise Exception(
+            f"Automatic checkpoint configuration: invalid \"steps_total\" format!", f"Got \"{config['steps_total']}\".")
+
+    if not isinstance(config["cfg"], float) or config["cfg"] < 0.0 or config["cfg"] > 100.0:
+        raise Exception(
+            f"Automatic checkpoint configuration: invalid \"cfg\" format! Got \"{config['cfg']}\".")
+
+    if not config["sampler_name"] in comfy.samplers.SAMPLER_NAMES:
+        raise Exception(
+            f"Automatic checkpoint configuration: invalid \"sampler_name\" format! Got \"{config['sampler_name']}\".")
+
+    if not config["scheduler_name"] in comfy.samplers.SCHEDULER_NAMES:
+        raise Exception(
+            f"Automatic checkpoint configuration: invalid \"scheduler_name\" format! Got \"{config['scheduler_name']}\".")
+
+
 # Globals
 script_dir = os.path.dirname(__file__)
 config_file = readConfigFile(os.path.join(script_dir, "models_config.yaml"))
@@ -37,7 +60,7 @@ class CheckpointAutomaticConfig(CheckpointLoaderSimple):
                     "step": 0.1,
                     }),
             "sampler_name": (comfy.samplers.SAMPLER_NAMES,),
-            "scheduler": (comfy.samplers.SCHEDULER_NAMES,),
+            "scheduler_name": (comfy.samplers.SCHEDULER_NAMES,),
         })
         return types
 
@@ -55,15 +78,22 @@ class CheckpointAutomaticConfig(CheckpointLoaderSimple):
         "SCHEDULER"
     )
 
-    def load_checkpoint(self, automatic_config, steps_total, cfg, sampler_name, scheduler, **kwargs):
+    def load_checkpoint(self, automatic_config, steps_total, cfg, sampler_name, scheduler_name, **kwargs):
         if automatic_config:
             if kwargs["ckpt_name"] in config_file:
+                # Validate first
+                validateCheckpointConfig(config_file[kwargs["ckpt_name"]])
+
+                # Assign new values
                 steps_total = config_file[kwargs["ckpt_name"]]["steps_total"]
                 cfg = config_file[kwargs["ckpt_name"]]["cfg"]
                 sampler_name = config_file[kwargs["ckpt_name"]]["sampler_name"]
-                scheduler = config_file[kwargs["ckpt_name"]]["scheduler"]
+                scheduler_name = config_file[kwargs["ckpt_name"]
+                                             ]["scheduler_name"]
+
+                # print assigned values
                 print(
-                    "======== Applying checkpoint automatic configuration: steps: {} | cfg: {} | sampler: {} | scheduler: {} ========".format(steps_total, cfg, sampler_name, scheduler))
+                    f"======== Applying checkpoint automatic configuration: steps: {steps_total} | cfg: {cfg} | sampler: {sampler_name} | scheduler: {scheduler_name} ========")
             else:
                 raise Exception(
                     "Automatic checkpoint configuration: unknown checkpoint. Disable \"automatic_config\" to use this checkpoint.")
@@ -73,7 +103,7 @@ class CheckpointAutomaticConfig(CheckpointLoaderSimple):
             steps_total,
             cfg,
             sampler_name,
-            scheduler
+            scheduler_name
         )
 
     CATEGORY = "Checkpoint Config Loader"
